108b2a210cf19270bb5153d3d0fddd9961afdea9
arpitfs <amarpitmalik204@gmail.com>
2021-01-05 17:08:45 +0530

    added test app for the sdk

---
+++ ServiceKit/ServiceKit.TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using ServiceKit.Sdk;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceKit.TestApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostUrl = "https://localhost:5001/";
+            var api = RestService.For<IServiceApi>(hostUrl);
+            var result = await api.GetService();
+            Console.WriteLine(result.Content.ServiceId);
+            Console.WriteLine(result.Content.ServiceName);
+            Console.ReadLine();
+        }
+    }
+}
